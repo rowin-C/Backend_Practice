ba4468bed734a98aff9e5689567a9855c3e25f2b
ron <saubhagyaprasad31122001@gmail.com>
2024-01-11 12:24:54 +0530

    utils code application

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.error(err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("DataBase connection error", err);
+  });
 
 /*
 import express from "express";
